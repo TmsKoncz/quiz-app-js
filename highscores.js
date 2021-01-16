f324bab70848d6e4455a4e9c11f1cb0fa6acd847
Koncz Tamás <55666304+koncztomi@users.noreply.github.com>
2021-01-16 20:05:04 +0100

    0

--- highscores.js
+++ highscores.js
@@ -1,5 +1,5 @@
 const highScoresList = document.querySelector('#highScoresList')
-const highScores = JSON.parse(sessionStorage.getItem("highScores")) || [] //ITT KELL VALAMIT ÁLLÍTANI localstorage az eredeti
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [] //ITT KELL VALAMIT ÁLLÍTANI localstorage az eredeti
 
 highScoresList.innerHTML =
 highScores.map(score => {
