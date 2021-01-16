2b8162f98b8fde86634bc841d1afc23cb545217b
Koncz Tamás <55666304+koncztomi@users.noreply.github.com>
2021-01-16 20:31:08 +0100

    0

--- game.js
+++ game.js
@@ -68,7 +68,7 @@ getNewQuestion = () => {
     if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
         localStorage.setItem('mostRecentScore', score)
 
-        return window.location.assign('https://tmskoncz.github.io/virusok-kviz/end.html')
+        return window.location.assign('https://tmskoncz.github.io/virusok-kviz/end')
     }
 
     questionCounter++
