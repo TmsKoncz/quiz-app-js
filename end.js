3d63ba688ef1c71a0e26d53a911e216c28d8ccff
Koncz Tamás <55666304+koncztomi@users.noreply.github.com>
2021-01-16 19:36:30 +0100

    Update end.js

--- end.js
+++ end.js
@@ -10,7 +10,7 @@ const MAX_HIGH_SCORES = 100
 finalScore.innerText = mostRecentScore
 
 username.addEventListener('keyup', () => {
-    saveScoreBtn.disabled = !username.value
+    saveScoreBtn.enabled = !username.value
 })
 
 saveHighScore = e => {
