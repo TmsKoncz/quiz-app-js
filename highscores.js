4389b4dae7a54874da7c2c8060494f35003862e3
Koncz Tamás <55666304+koncztomi@users.noreply.github.com>
2021-01-16 19:48:30 +0100

    Update highscores.js

--- highscores.js
+++ highscores.js
@@ -1,7 +1,7 @@
 const highScoresList = document.querySelector('#highScoresList')
-const highScores = JSON.parse(localStorage.getItem("highScores")) || []
+const highScores = JSON.parse(sessionStorage.getItem("highScores")) || [] //ITT KELL VALAMIT ÁLLÍTANI localstorage az eredeti
 
 highScoresList.innerHTML =
 highScores.map(score => {
-    return `<li class="high-score">${score.name} - ${score.score}</li>`
+    return `<li class="high-score">${score.name} - ${score.score} pont</li>`
 }).join("")
