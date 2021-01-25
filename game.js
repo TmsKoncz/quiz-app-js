34f44bd456cca0f78675d9a6d343832f652c0634
Koncz Tamás <55666304+koncztomi@users.noreply.github.com>
2021-01-25 13:39:02 +0100

    Update game.js

--- game.js
+++ game.js
@@ -139,11 +139,6 @@ startGame()
     highScores.push(score) }
     }
 
-        // Update remaining seconds
-        document.getElementById("countdown").innerHTML = seconds;
-        // Count down using javascript
-        window.setTimeout("countdown()", 1000);
-
 saveHighScore = e => {
             e.preventDefault()
         
