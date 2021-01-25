3dbbca71b8ef18b5f2d0508a4f27e8d83bd144ed
Koncz Tamás <55666304+koncztomi@users.noreply.github.com>
2021-01-25 13:21:21 +0100

    Update game.js

--- game.js
+++ game.js
@@ -145,38 +145,3 @@ startGame()
         window.setTimeout("countdown()", 1000);
 
 // COUNTDOWN
-function countdown( countdown, minutes, seconds ) //elementname van eredetile a cd. helyett
-{
-    var element, endTime, hours, mins, msLeft, time;
-
-    function twoDigits( n )
-    {
-        return (n <= 9 ? "0" + n : n);
-    }
-
-    function updateTimer()
-    {
-        msLeft = endTime - (+new Date);
-        if ( msLeft < 1000 ) {
-            element.innerHTML = "Az idő lejárt!";
-        } else {
-            time = new Date( msLeft );
-            hours = time.getUTCHours();
-            mins = time.getUTCMinutes();
-            element.innerHTML = (hours ? hours + ':' + twoDigits( mins ) : mins) + ':' + twoDigits( time.getUTCSeconds() );
-            setTimeout( updateTimer, time.getUTCMilliseconds() + 500 );
-        }
-    }
-
-    element = document.getElementById( countdown );
-    endTime = (+new Date) + 1000 * (60*minutes + seconds) + 500;
-    updateTimer();
-}
-
-countdown( "countdown", 0, 30 );
-
-
-function hideCountdown(){
-    $(x-btn).click(function (event) {
-        $('#countdown:visible').hide();
-})};
